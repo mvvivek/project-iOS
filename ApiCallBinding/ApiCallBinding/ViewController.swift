5d2e5f6a114e04dadf4a685460d1adb1e838246f
mvvivek <vivek.mv@kooapp.com>
2024-01-05 15:51:40 +0530

    New-proj-api-call-binding-added

---
+++ ApiCallBinding/ApiCallBinding/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApiCallBinding
+//
+//  Created by Vivek MV on 05/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
